319c90e2905c4384df7aea37492afc28338b57a8
musawakiliML <musawakiliml@hotmail.com>
2022-09-04 21:58:41 +0100

    Created quotes scraper to get text author and tags with pagination

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -7,7 +7,24 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        heading_tag = response.xpath("//h1/a/text()").extract_first()
-        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+       # heading_tag = response.xpath("//h1/a/text()").extract_first()
+       # tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
 
-        yield {'Heading tag': heading_tag, "Tags": tags}
+        #  yield {'Heading tag': heading_tag, "Tags": tags}
+
+        # getting all the quotes
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text = quote.xpath('.//*[@itemprop="text"]/text()').extract()
+            author = quote.xpath('.//*[@itemprop="author"]/text()').extract()
+            tags = quote.xpath('.//*[@class="tag"]/text()').extract()
+
+            print('\n')
+            print(text)
+            print(author)
+            print(tags)
+            print('\n')
+
+        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield scrapy.Request(absolute_next_page_url)
